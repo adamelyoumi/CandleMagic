fa661ca12f8e430c6b32510a26da31b4e61e2c66
adamelyoumi <adam.elyoumi@hotmail.fr>
2024-01-04 03:59:32 +0100

    added MSS, FVG, utilities

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from datetime import datetime
 import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import numpy as np
+import bisect
 
 class Candle():
     def __init__(self, t, tf, h, l, o, c) -> None:
@@ -10,18 +13,27 @@ class Candle():
         self.low = l
         self.open = o
         self.close = c
-        
-        if self.open == self.close:
-            self.dir = None
-        else:
-            self.dir = self.open < self.close # True = Bull, False = Bear, None = None
+            
+    def isBullish(self):
+        return(self.open > self.close)
+    
+    def isBearish(self):
+        return(self.open < self.close)
+
+    def bodySize(self):
+        return(abs(self.open-self.close))
         
 class Market():
-    def __init__(self, file) -> None:
+    def __init__(self, file, ma=10, ema=10) -> None:
         self.data = pd.read_csv(file, sep="\t")
+        
         self.data['<DATETIME>'] = pd.to_datetime(self.data['<DATE>'] + ' ' + self.data['<TIME>'], format='%Y.%m.%d %H:%M:%S')
         self.data = self.data.drop(['<DATE>', '<TIME>', '<VOL>', '<SPREAD>' ], axis=1)
         self.data = self.data[['<DATETIME>'] + [col for col in self.data.columns if col != '<DATETIME>']]
+        
+        self.data['SMA'] = self.data['Price'].rolling(window=ma).mean()
+        self.data['EMA'] = self.data['Price'].ewm(span=ema, adjust=False).mean()
+        
         self.chart : list[Candle] = []
         self.n_candles = self.data.shape[0]
         self.start_date = min(self.data["<DATETIME>"])
@@ -33,20 +45,50 @@ class Market():
             candle = Candle(data["Date"], self.tf, data["High"], data["Low"], data["Open"], data["Close"])
             self.chart.append(candle)
             
-    def get_FVGs(self):
+        uptrends =   ma*[np.nan] + [ (sum([ int(self.chart[i].isBullish()) for i in range(k-ma, k) ]) > int(ma*0.75)) for k in range(ma, self.n_candles) ]
+        downtrends = ma*[np.nan] + [ (sum([ int(self.chart[i].isBearish()) for i in range(k-ma, k) ]) > int(ma*0.75)) for k in range(ma, self.n_candles) ]
+        
+        self.data["Uptrend"] = pd.Series(uptrends)
+        self.data["Downtrend"] = pd.Series(downtrends)
+        
+        self.swi = {"high":[], "low": []}
+        
+        for k in range(1,self.n_candles-1):
+            if self.chart[k].high > self.chart[k-1].high and self.chart[k].high > self.chart[k+1].high:
+                self.swi["high"].append(k)
+            if self.chart[k].low < self.chart[k-1].low and self.chart[k].low < self.chart[k+1].low:
+                self.swi["low"].append(k)
+            
+    def getFVGs(self, return_times = False):
         """
         Returns the ??? of the market's FVGs
         """
         
-        FVGs = {"bull":[], "bear": []}
+        fvg = {"bull":[], "bear": []}
         
         for k in range(1,self.n_candles-1):
             if self.chart[k-1].high < self.chart[k+1].low:
-                FVGs["bull"].append(self.chart[k].timestamp)
+                fvg["bull"].append((self.chart[k].timestamp if return_times else k))
+                
             if self.chart[k-1].low > self.chart[k+1].high:
-                FVGs["bear"].append(self.chart[k].timestamp)
+                fvg["bear"].append((self.chart[k].timestamp if return_times else k))
         
-        return(FVGs)
+        return(fvg)
+    
+    def getMSSs(self, return_times = False):
+        
+        mss = {"bull":[], "bear": []}
+        
+        for k in range(10,self.n_candles-2):
+            lastSwingHigh = self.chart[bisect.bisect_left(self.swi["high"], k)]
+            lastSwingLow  = self.chart[bisect.bisect_left(self.swi["low"],  k)]
+            
+            if self.data["Uptrend"][k] and self.chart[k+1].isBearish() and self.chart[k+2].isBearish() and self.chart[k].close > lastSwingHigh.high:
+                mss["bear"].append((self.chart[k].timestamp if return_times else k))
+            if self.data["Downtrend"][k] and self.chart[k+1].isBullish() and self.chart[k+2].isBullish() and self.chart[k].close < lastSwingLow.low:
+                mss["bull"].append((self.chart[k].timestamp if return_times else k))
+                
+        
 
     def get_OBs(self, dir):
         OBs = {"bull":[], "bear": []}
@@ -59,7 +101,7 @@ class Market():
         
         return(OBs)
     
-    def plot(self, start = None, end = None, FVG = False):
+    def plot(self, start = None, end = None, FVG = False, MSS = False, EMA = False):
         
         start = start if start else self.start_date
         end = end if end else self.end_date
@@ -71,6 +113,8 @@ class Market():
                             high=df['<HIGH>'],
                             low=df['<LOW>'],
                             close=df['<CLOSE>']))
+        
+        fig.add
         
         if FVG:
             fig.add_scatter(...)
